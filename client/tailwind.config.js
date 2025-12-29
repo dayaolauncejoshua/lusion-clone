0f6d848a3b9624b7efd3e133a5bd7b89cdc3a738
dayaolauncejoshua <dayaolauncejoshua@gmail.com>
2025-12-29 12:36:38 +0800

    asd

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        dark: '#0a0a0a',
-        light: '#f5f5f5',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
